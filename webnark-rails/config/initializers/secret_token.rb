cd5503998468e27faa71643bf40cce94db77f875
Fraser Scott <fraser.scott@burberry.com>
2013-07-30 10:40:51 +0100

    created new rails app and added icons to design test

---
+++ webnark-rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebnarkRails::Application.config.secret_key_base = 'fd5a745b02fa1f29c11244645735d6cfc542e50c5720f38a1dffc3cd64ec113db226a0fbe83734e41abe2558fb42e62723d178e57a2ba41647a00cbb28708daa'
---
+++ webnark-rails/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WebnarkRails::Application.config.session_store :cookie_store, key: '_webnark-rails_session'
