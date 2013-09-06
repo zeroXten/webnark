04ecef323af50c66026ebfed5e7c8ba6f11a719b
Fraser Scott <fraser.scott@burberry.com>
2013-09-06 22:57:53 +0100

    working screenshots, needs hardening in prod

--- webnark-rails/app/workers/screenshot_worker.rb
+++ webnark-rails/app/workers/screenshot_worker.rb
@@ -2,6 +2,17 @@ class ScreenshotWorker
   include Sidekiq::Worker
 
   def perform(url, slug)
-    `/Users/fscott/Personal/projects/webnark/screenshot/make_screenshot.sh "#{url}" > /Users/fscott/Personal/projects/webnark/webnark-rails/public/screenshots/#{slug}.png`
+    image_path = "/Users/fscott/Personal/projects/webnark/webnark-rails/public/screenshots"
+
+    image = `/Users/fscott/Personal/projects/webnark/screenshot/make_screenshot.sh '#{url}'`
+
+    if not image or image.empty?
+      FileUtils.cp("#{image_path}/_na.png","#{image_path}/#{slug}.png")
+    else
+      f = File.new("#{image_path}/#{slug}.png", "wb")
+      f.write(image)
+      f.close
+    end
   end
+
 end
